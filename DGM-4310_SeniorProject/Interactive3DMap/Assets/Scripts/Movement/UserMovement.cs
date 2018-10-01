7b24b5e3fba94ae1b9ad4fbc0f0fdeb7d2b1ec79
Unknown <colin.hite@gmail.com>
2018-10-01 12:32:00 -0600

    Got a basic movement script working

--- DGM-4310_SeniorProject/Interactive3DMap/Assets/Scripts/Movement/UserMovement.cs
+++ DGM-4310_SeniorProject/Interactive3DMap/Assets/Scripts/Movement/UserMovement.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class UserMovement : MonoBehaviour {
 
+    public float speed = 10.0f;
+    public GameObject user;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    if(Input.GetKey (KeyCode.W))
+	        {
+	            user.transform.Translate(0, 0, (speed * Time.deltaTime));
+	            //Debug.Log("I pressed the button");
+	        }
+	    if(Input.GetKey (KeyCode.A))
+	        {
+	            user.transform.Translate((speed * Time.deltaTime), 0, 0);
+	            //Debug.Log("I pressed the button");
+	        }
+	    if(Input.GetKey (KeyCode.S))
+	        {
+	            user.transform.Translate(0, 0, (speed * Time.deltaTime * -1));
+	            //Debug.Log("I pressed the button");
+	        }
+	    if(Input.GetKey (KeyCode.D))
+	        {
+	            user.transform.Translate((speed * Time.deltaTime * -1), 0, 0);
+	            //Debug.Log("I pressed the button");
+	        }
+	    //user.Transform.translate = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 	}
 }
