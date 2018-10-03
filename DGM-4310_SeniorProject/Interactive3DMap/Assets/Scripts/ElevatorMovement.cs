d6d9a2dad2be2f27bbb7fa8d7da0f336889400bc
Unknown <colin.hite@gmail.com>
2018-10-03 12:34:13 -0600

    Started the elevator script
    Note you should also work on the look script

---
+++ DGM-4310_SeniorProject/Interactive3DMap/Assets/Scripts/ElevatorMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMovement : MonoBehaviour {
+
+    public GameObject elevator;
+    public array floors;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// If the elevator is at the set floor then dont run
+	if ()
+	// If the elevator is not at the set floor then translate its position
+		elevator.transform.Translate(0, 0, (speed * Time.deltaTime));
+	}
+}
